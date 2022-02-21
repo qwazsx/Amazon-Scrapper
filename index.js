35a637b69e84c634349f1905e446a6b4de81cd8a
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-21 22:56:30 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,83 @@
+// npm install express request request-promise nodemon
+// nodemon'u her yenilendiğinde tekrar render etsin diye koyduk
+// heroku üzerinde hostlayacağız
+
+const express = require("express");
+const request = require("request-promise");
+
+//app initialization
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// https://dashboard.scraperapi.com/dashboard buradan aldık
+//const baseUrl = `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+const generateScraperUrl = (apiKey) => `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+// expres json kullanabilsin diye ekledik
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API')
+})
+
+//first route
+// Get Product Details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params
+    const { api_key } = req.query
+
+    try {
+        const response = await request(`${generateScraperUrlgenerateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`)
+
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+// Get Product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params
+    const { api_key } = req.query
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`)
+
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+// Get Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params
+    const { api_key } = req.query
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+// Get Search Results
+app.get('/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params
+    const { api_key } = req.query
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`)
+
+        res.json(JSON.parse(response))
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+//npm run dev ile çalıştırdık
